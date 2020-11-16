2b84bd4fca06c73c75fa3d3c85fd4f59ad7edd02
unknown <jonatas-flash@hotmail.com>
2020-11-16 18:21:37 -0300

    start project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,126 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header class="header">
+      <h1>Rock X Paper X Scissors</h1>
+
+    </header>
+    
+
+    <div class="game">
+
+      <div class="players"> teste x teste </div>
+
+      <img class="icon -rock" src="./img/rock.svg" alt="Rock (pedra)">
+      <img class="icon -paper" src="./img/paper.svg" alt="Paper (papel)">
+      <img class="icon -scissors" src="./img/scissors.svg" alt="Scissors (tesoura)">
+
+      <aside class="right">
+        <p>...</p>
+      </aside>
+    </div>
+
+    <footer class="footer">
+      <a href="https://www.vecteezy.com/free-vector/rock-paper-scissors">Rock Paper Scissors Vectors by Vecteezy</a>
+    </footer>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
 
 <style>
+
+body {
+  margin: 0;
+  background-color: #2f1a52;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #d1d1d1;
+  
+
+
+  
+
+  position: relative;
+  display: grid;
+  grid-template-areas: 
+  "header header header header"
+  "game game game game"
+  "footer footer footer footer"
+  ;
+}
+
+.header {
+  background-color: brown;
+  height: 100px;
+  width: 100%;
+
+  grid-area: header;
+}
+
+.game {
+  grid-area: game;
+
+  height: calc(100vh - 200px);
+  width: 100vw;
+
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr minmax(200px, 1fr);
+  grid-template-areas: 
+    "players players players right"
+    "icon1 icon2 icon3 right"
+  ;
+}
+
+.right {
+  background-color: rgb(78, 78, 78);
+  
+  /*height: 100%;*/
+
+  grid-area: right;
+}
+.players {
+  /*position: absolute;*/
+  width: 100%;
+  height: 50px;
+  background-color: rgba(119, 136, 153, .8);
+  grid-area: players;
+}
+
+.icon {
+  width: 130px;
+  /*height: 100%;*/
+  cursor: pointer;
+
+  
+
+}
+
+
+
+.footer {
+  background-color: brown;
+  height: 100px;
+  width: 100%;
+
+  position: fixed;
+  bottom: 0;
+  left: 0;
+
+  /*position: absolute;
+  bottom: 0;*/
+
+  grid-area: footer;
 }
 </style>
